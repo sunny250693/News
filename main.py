36ea375ae5fd9cf0b99b59a9bb6ac47defa6f00d
Aamir Khan <aamir.k56@gmail.com>
2017-05-01 00:50:47 +0530

    Correct Typo

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ app = Flask(__name__)
 def HelloWorld():
     return "Hello World"
 
-if ___name__ == '__main__':
+if __name__ == '__main__':
     app.debug = True
     app.run(host="0.0.0.0", port=80)
