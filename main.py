d5cb7bb17c6f0f3485d9ab958773f208852e1f58
Aamir Khan <aamir.k56@gmail.com>
2017-05-02 00:54:51 +0530

    Render index page

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/hello')
-def HelloWorld():
-    return "Hello World"
+@app.route('/index')
+def index_page():
+    return render_template('index.html', title="Sunny's Site")
 
 if __name__ == '__main__':
     app.debug = True
